04553b50c4841216121f63310df7ebf7116ea7c1
EuanPollock <euan.pollock@outlook.com>
2026-08-01 15:17:39 +0100

    Update and rename dashboardshell.tsx to applayout.tsx

--- app/components/applayout.tsx
+++ app/components/applayout.tsx
@@ -3,26 +3,32 @@
 import Sidebar from "./sidebar";
 import Topbar from "./topbar";
 
-type DashboardShellProps = {
+type AppLayoutProps = {
   children: React.ReactNode;
   user?: string;
 };
 
-export default function DashboardShell({
+export default function AppLayout({
   children,
   user,
-}: DashboardShellProps) {
+}: AppLayoutProps) {
   return (
     <div className="flex min-h-screen bg-slate-100">
+
       <Sidebar />
 
       <div className="flex flex-1 flex-col">
+
         <Topbar user={user} />
 
-        <main className="flex-1 p-8">
-          {children}
+        <main className="flex-1 overflow-y-auto p-8">
+          <div className="mx-auto max-w-7xl">
+            {children}
+          </div>
         </main>
+
       </div>
+
     </div>
   );
 }
